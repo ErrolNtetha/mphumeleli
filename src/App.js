b87424660aa6f016a9669307b5d83681273e75d2
ErrolNtetha <mphumier@gmail.com>
2021-02-18 15:34:39 +0200

    Made changes to my site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import NavBar from "./components/Header/NavBar";
+import Home from './components/Pages/Home'
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   );
 }
